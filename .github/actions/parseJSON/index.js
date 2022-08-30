ad3210f75aebf6247b6f2889cd6eea4b12d06850
SvataSejkora <sejkorasvatopluk@gmail.com>
2022-08-30 19:44:54 +0200

    trying node app

--- .github/actions/parseJSON/index.js
+++ .github/actions/parseJSON/index.js
@@ -3,21 +3,31 @@ const fs = require('fs');
 
 try {
     const branch = core.getInput('branch');
-    const certificateRaw = core.getInput('certificate')
     let rawData = fs.readFileSync('.github/settings/repositoryConfig.json');
     let data = JSON.parse(rawData);
 
-    let certificates = JSON.parse(certificateRaw);
-    console.log(certificates);
-
-    const decodedStringAtoB = atob(certificates.develop);
-    console.log(decodedStringAtoB);
-
     const branchData = data[branch];
+
+    let certificatePath;
+    switch (branch) {
+        case 'develop':
+            certificatePath = '${HOME}/qa.key';
+            break;
+        case 'staging':
+            certificatePath = '${HOME}/staging.key';
+            break;
+        case 'main':
+            certificatePath = '${HOME}/prod.key';
+            break;
+        default:
+            certificatePath = '${HOME}/qa.key';
+    }
+
 
     core.setOutput('userName', branchData.userName);
     core.setOutput('clientId', branchData.clientId);
     core.setOutput('instanceUrl', branchData.instanceUrl);
+    core.setOutput('certificatePath', certificatePath);
 
 } catch (e) {
     core.setFailed(e.message);
