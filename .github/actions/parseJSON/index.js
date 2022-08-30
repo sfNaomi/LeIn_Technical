8fd4554519ed4e0106b6a0bebb1180a4ec9ef026
SvataSejkora <sejkorasvatopluk@gmail.com>
2022-08-30 19:06:12 +0200

    trying node app

--- .github/actions/parseJSON/index.js
+++ .github/actions/parseJSON/index.js
@@ -2,15 +2,19 @@ const core = require("@actions/core");
 const fs = require('fs');
 
 try {
-    const branch = core.getInput("branch");
+    const branch = core.getInput('branch');
+    const certificateRaw = core.getInput('certificate')
     let rawData = fs.readFileSync('.github/settings/repositoryConfig.json');
     let data = JSON.parse(rawData);
 
+    let certificates = JSON.parse(certificateRaw);
+    console.log(certificates);
+
     const branchData = data[branch];
 
-    core.setOutput("userName", branchData.userName);
-    core.setOutput("clientId", branchData.clientId);
-    core.setOutput("instanceUrl", branchData.instanceUrl);
+    core.setOutput('userName', branchData.userName);
+    core.setOutput('clientId', branchData.clientId);
+    core.setOutput('instanceUrl', branchData.instanceUrl);
 
 } catch (e) {
     core.setFailed(e.message);
