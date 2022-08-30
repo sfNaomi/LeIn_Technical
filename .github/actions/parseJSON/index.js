9aec950afe6516cd19e95f9f19b2a749a7d7f94f
SvataSejkora <sejkorasvatopluk@gmail.com>
2022-08-30 18:18:24 +0200

    trying node app

--- .github/actions/parseJSON/index.js
+++ .github/actions/parseJSON/index.js
@@ -1,21 +1,16 @@
-console.log('is this really gonna work?');
-
 const core = require("@actions/core");
-//const github = require("@actions/github");
 const fs = require('fs');
 
 try {
     const branch = core.getInput("branch");
     let rawData = fs.readFileSync('.github/settings/repositoryConfig.json');
     let data = JSON.parse(rawData);
-    console.log(data);
 
-    console.log(branch);
     const branchData = data[branch];
 
     core.setOutput("userName", branchData.userName);
-    //core.setOutput("clientId", lastSuccessCommitHash);
-    //core.setOutput("instanceUrl", lastSuccessCommitHash);
+    core.setOutput("clientId", branchData.clientId);
+    core.setOutput("instanceUrl", branchData.instanceUrl);
 
 } catch (e) {
     core.setFailed(e.message);
