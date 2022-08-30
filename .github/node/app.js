76b0b60d4817992fbd884d783f2ddae44f81ae3a
SvataSejkora <sejkorasvatopluk@gmail.com>
2022-08-30 16:52:13 +0200

    trying node app

--- .github/node/app.js
+++ .github/node/app.js
@@ -1,1 +1,30 @@
 console.log('is this really gonna work?');
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require('fs');
+
+try {
+    const branch = core.getInput("branch");
+    let file;
+    fs.readFile('.github/settings/repositoryConfig.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(data);
+        file = data;
+    });
+
+    const object = JSON.parse(file);
+    console.log(object);
+
+    //core.setOutput("username", lastSuccessCommitHash);
+    //core.setOutput("clientId", lastSuccessCommitHash);
+    //core.setOutput("instanceUrl", lastSuccessCommitHash);
+
+} catch (e) {
+    core.setFailed(e.message);
+}
+
+
+
