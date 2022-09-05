2a1f3f95f91ffc907741c7f4b51ab8e2168476b0
magdalena-stanciu-bpt <63054679+magdalena-stanciu-bpt@users.noreply.github.com>
2022-09-06 01:46:50 +0300

    PF-59: Added LWC

---
+++ src/main/default/lwc/recordPageWarningMessage/recordPageWarningMessage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by magdalena.stanciu on 05.09.2022.
+ */
+
+import { LightningElement, api } from 'lwc';
+
+export default class RecordPageWarningMessage extends LightningElement {
+    @api message;
+    @api variant;
+
+    get iconName() {
+        return 'utility:' + this.variant.toLowerCase();
+    }
+}
