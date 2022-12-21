17f662f7a7739f7da6df47682f187dc8c5278019
Svatopluk Sejkora <100215457+SvataSejkoraBearingPoint@users.noreply.github.com>
2022-12-21 20:18:12 +0100

    PF-703 - remove load link when order is set to unplanned via logistic… (#568)
    * PF-703 - remove load link when order is set to unplanned via logistic screen. Allow Depot Operations to modify order products
    
    * fix name
    
    Co-authored-by: SvataSejkora <sejkorasvatopluk@gmail.com>

--- src/main/default/lwc/logisticUpdateScreen/logisticUpdateScreen.js
+++ src/main/default/lwc/logisticUpdateScreen/logisticUpdateScreen.js
@@ -329,7 +329,7 @@ export default class LogisticUpdateScreen extends NavigationMixin(LightningEleme
                     this.updateRecordFields(this.getFieldFromSelectedOrders('Id'), {'Status': 'Cancelled', 'DeliveryFailed__c': true});
                     break;
                 case 'Replan':
-                    this.updateRecordFields(this.getFieldFromSelectedOrders('Id'), {'Status': 'Unplanned', 'Replanned__c': true});
+                    this.updateRecordFields(this.getFieldFromSelectedOrders('Id'), {'Status': 'Unplanned', 'Replanned__c': true, 'Load__c': null});
                     break;
                 default:
             }
